9e2683fc6c7418c13b4d5677f88a33b76dbd65c3
Yashvi68 <Yashvi.khandelwal@reynasolutions.com>
2026-02-11 17:36:24 +0530

    configured vite file allowing jsx syntax in .js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+      // This option tells the plugin to process both .js and .jsx files
+      // for Babel or esbuild transformations.
+      include: '**/*.{jsx,js}',
+    })],
 })
